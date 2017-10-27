77d65c3afa326cac8cd2c57e959ed4c97c411131
Łukasz Brze <neox210@gmail.com>
2017-10-27 11:55:25 +0200

    some cosmetic changes after a realy busy month. After 03.11.2017 updates will be const.

---
+++ Korepetycje/Controllers/api/StudentEventsController.cs
@@ -0,0 +1,26 @@
+﻿using Korepetycje.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Korepetycje.Controllers.api
+{
+    public class StudentEventsController : ApiController
+    {
+        private ApplicationDbContext context;
+
+        public StudentEventsController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public IEnumerable<CallendarEvents> GetEvents()
+        {
+            var userId = User.Identity.GetUserId();
+            var events = context.CallendarEvents.Where(e => e.StudentId == userId).ToList();
+            return events;
+        }
+    }
+}
